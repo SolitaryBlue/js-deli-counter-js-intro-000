f003a65f7277c899b54f095e60e9b4de5f1d0b64
JP Luttrell <jpluttrell@protonmail.com>
2020-05-24 23:28:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
+
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine === []) {
+    return "There is nobody waiting to be served!"
+  }
+}
