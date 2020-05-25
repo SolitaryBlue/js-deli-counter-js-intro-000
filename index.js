3bd11ee43d190f2a1d7a742d3466cb3cd37b54d2
JP Luttrell <jpluttrell@protonmail.com>
2020-05-25 00:09:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(katzDeli) {
   } else {
     let peopleInLine = [];
     for (let i = 0; i < katzDeli.length; i++) {
-      peopleInLine.push(katzDeli[i])
+      peopleInLine.push(katzDeli[i].length)
     }
     return `The line is currently: ${katzDeli.length}. ${peopleInLine}`;
   }
